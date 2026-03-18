07cad99469ed702b4ab6bc5a4d4e5f1491626f19
unknown <matheuscostaesilva12@gmail.com>
2026-03-18 19:49:56 -0300

    Service criada

--- atividade6/src/main/java/com/atividade/atividade6/controllers/ClienteController.java
+++ atividade6/src/main/java/com/atividade/atividade6/controllers/ClienteController.java
@@ -1,13 +1,41 @@
 package com.atividade.atividade6.controllers;
 
+import com.atividade.atividade6.models.ClienteModel;
+import com.atividade.atividade6.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/clientes")
 public class ClienteController {
 
     @Autowired
+    ClienteService clienteService;
 
+    @PostMapping
+    public ClienteModel adicionarCliente(@RequestBody ClienteModel clienteModel) {
+        return clienteService.criarCliente(clienteModel);
+    }
+
+    @GetMapping
+    public List<ClienteModel> listarClientes(){
+        return clienteService.listarClientes();
+    }
+
+    @GetMapping
+    public List<ClienteModel> listarClientesPorId(Long id) {
+        return clienteService.listarClientesPorId(id);
+    }
+
+    @DeleteMapping
+    public void deletarCliente(@PathVariable Long id) {
+        clienteService.deletarCliente(id);
+    }
+
+    @PutMapping
+    public ClienteModel atualizarCliente(@PathVariable Long id, @RequestBody ClienteModel clienteModel) {
+        return clienteService.atualizarCliente(id, clienteModel);
+    }
 }

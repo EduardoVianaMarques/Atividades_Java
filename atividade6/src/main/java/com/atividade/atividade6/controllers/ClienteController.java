158bce64d54e18c943b09c2e295211af26f9cd6f
unknown <matheuscostaesilva12@gmail.com>
2026-03-18 19:45:36 -0300

    Service criada

---
+++ atividade6/src/main/java/com/atividade/atividade6/controllers/ClienteController.java
@@ -0,0 +1,13 @@
+package com.atividade.atividade6.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/clientes")
+public class ClienteController {
+
+    @Autowired
+
+}
---
+++ atividade6/src/main/java/com/atividade/atividade6/services/ClienteService.java
@@ -0,0 +1,40 @@
+package com.atividade.atividade6.services;
+
+import com.atividade.atividade6.controllers.ClienteController;
+import com.atividade.atividade6.models.ClienteModel;
+import com.atividade.atividade6.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@Service
+public class ClienteService {
+
+    private static final Logger log = Logger.getLogger(ClienteService.class);
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public ClienteModel criarCliente(ClienteModel clienteModel) {
+        return clienteRepository.save(clienteModel);
+    }
+
+    public List<ClienteModel> listarClientes() {
+        return clienteRepository.findAll();
+    }
+
+    public List<ClienteModel> listarClientesPorId(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    public void deletarCliente(Long id) {
+        clienteRepository.deleteById(id);
+    }
+
+    public ClienteModel atualizarCliente(Long id, ClienteModel clienteModel) {
+        ClienteModel newCliente = clienteRepository.findById(id).get();
+        newCliente.setNome(clienteModel.getNome());
+        return clienteRepository.save(newCliente);
+    }
+}

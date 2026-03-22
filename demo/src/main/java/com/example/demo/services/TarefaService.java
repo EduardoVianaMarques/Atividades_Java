367e00c943b381ac95b40659750a2fc4d30ab4b9
MatheusCostaeSilva <matheuscostaesilva12@gmail.com>
2026-03-22 15:11:26 -0300

    service criado

---
+++ demo/src/main/java/com/example/demo/services/TarefaService.java
@@ -0,0 +1,42 @@
+package com.example.demo.services;
+
+import com.example.demo.models.TarefaModel;
+import com.example.demo.repositories.TarefaRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TarefaService {
+
+    public static final Logger log = LoggerFactory.getLogger(TarefaService.class.getName());
+
+    @Autowired
+    private TarefaRepository tarefaRepository;
+
+    public TarefaModel criarTarefa(TarefaModel tarefaModel) {
+        return tarefaRepository.save(tarefaModel);
+    }
+
+    public List<TarefaModel> listarTarefa() {
+        return tarefaRepository.findAll();
+    }
+
+    public Optional<TarefaModel> buscarId(Long id) {
+        return tarefaRepository.findById(id);
+    }
+
+    public void deletarTarefa(Long id) {
+        tarefaRepository.deleteById(id);
+    }
+
+    public TarefaModel atualizarTarefa(Long id, TarefaModel tarefaModel) {
+        TarefaModel newTarefa = tarefaRepository.findById(id).get();
+        newTarefa.setDescricao(tarefaModel.getDescricao());
+        return tarefaRepository.save(newTarefa);
+    }
+}

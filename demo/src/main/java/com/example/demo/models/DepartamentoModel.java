9d74d652c34a19fc36c1e976e7792afe7bfb373f
MatheusCostaeSilva <matheuscostaesilva12@gmail.com>
2026-03-22 14:29:54 -0300

    model criado

--- demo/src/main/java/com/example/demo/models/DepartamentoModel.java
+++ demo/src/main/java/com/example/demo/models/DepartamentoModel.java
@@ -1,7 +1,46 @@
 package com.example.demo.models;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "tb_departamento")
 public class DepartamentoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    private String localizacao;
+
+    public DepartamentoModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
 }

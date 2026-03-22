90d34bf7f0471ae01d2b13d4bd0c84d6b20b738a
EduardoVianaMarques <duduvmarques@gmail.com>
2026-03-22 15:42:30 -0300

    Model Finalizado

---
+++ src/main/java/com/Gerenciador_Categorias/Gerenciador_Categorias/GerenciadorCategoriasApplication.java
@@ -0,0 +1,13 @@
+package com.Gerenciador_Categorias.Gerenciador_Categorias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorCategoriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorCategoriasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Gerenciador_Categorias/Gerenciador_Categorias/GerenciadorCategoriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gerenciador_Categorias.Gerenciador_Categorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorCategoriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
